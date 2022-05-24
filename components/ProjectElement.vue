bccb57a9de3a9462050c7b5c23108311aa575416
Aurelien <aurelien@MacBook-Pro-de-Aurelien.local>
2022-05-25 01:35:07 +0200

    adding dark style

--- components/ProjectElement.vue
+++ components/ProjectElement.vue
@@ -4,7 +4,7 @@
     <div class="w-full flex justify-center max-w-3xl">
       <div class="w-11/12 flex flex-col place-items-center ">
         <a :href="git"><img :src="link" class="rounded-xl min-h-[165px] " alt=""></a>
-        <h1 class="font-semibold text-2xl text-white">{{ title }}</h1>
+        <h1 class="font-semibold text-2xl text-white dark:text-black">{{ title }}</h1>
         <p class="text-center text-gray-500 text-sm font-semibold">{{ description }}</p>
 
       </div>
--- components/ProjectMain.vue
+++ components/ProjectMain.vue
@@ -1,7 +1,7 @@
 <template class="flex justify-center h-full bg-[#202023] dark:bg-amber-100">
   <div class="flex flex-col place-items-center bg-[#202023] dark:bg-amber-100 h-full">
     <div class="max-w-3xl w-full pl-7">
-      <h1 class="_fullname font-semibold text-white text-xl border-b-4 border-gray-600 w-32 mb-4 mt-5 ">Mes projets</h1>
+      <h1 class="_fullname font-semibold text-white text-xl border-b-4 border-gray-600 w-32 mb-4 mt-5 dark:text-black ">Mes projets</h1>
     </div>
 
     <div class="p-7 max-w-3xl md:grid md:grid-cols-2 h-full bg-[#202023] dark:bg-amber-100">
